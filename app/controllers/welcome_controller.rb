ad3231e4702fa03d19499121c51007938a721133
originalblend337 <originalblend337@gmail.com>
2013-10-30 17:23:52 -0400

    added user model

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,8 +4,10 @@ class WelcomeController < ApplicationController
   end
 
   def callback
-  	response = RestClient.post("https://github.com/login/oauth/access_token", {client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code: params["code"]}, { accept: :json })
-  	parsed_response = JSON.parse(response)
+  	# Get access token from code param
+    response = RestClient.post("https://github.com/login/oauth/access_token", {client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code: params["code"]}, { accept: :json })
+  	parsed_response = JSON.parse(response))
+
   	# current_user is a method in devise / github_access_token to be a field in the user model
   	current_user.github_access_token = parsed_response["access_token"]
   	redirect_to root_path
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131030013324) do
+ActiveRecord::Schema.define(version: 20131030184902) do
 
   create_table "repositories", force: true do |t|
     t.string   "name"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20131030013324) do
     t.string   "language"
     t.string   "owner"
     t.string   "avatar"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "github_access_token"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
