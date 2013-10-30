f0c83d5f0b10a7bed8b9f5fd50427e776563b35c
originalblend337 <originalblend337@gmail.com>
2013-10-30 01:51:06 -0400

    minor changes

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -2,9 +2,12 @@ class RepositoryController < ApplicationController
 
 	def index
 		@repositories = Repository.get_repos(params[:user_name])
+
+		# Save the user repo info to Repository model (cache it)
   		@repositories.each do |fetched_repository|
   			repository.create(name: fetched_repository[:name], description: fetched_repository[:description], owner: fetched_repository[:owner])
   		end
+  		
 		@languages = Repository.sort_repos(@repositories)
 	end
 
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,12 @@
 class WelcomeController < ApplicationController
   def index
+  	# Thanks for logging in?
   end
 
   def callback
   	response = RestClient.post("https://github.com/login/oauth/access_token", {client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code: params["code"]}, { accept: :json })
   	parsed_response = JSON.parse(response)
+  	# current_user is a method in devise / github_access_token to be a field in the user model
   	current_user.github_access_token = parsed_response["access_token"]
   	redirect_to root_path
   end
