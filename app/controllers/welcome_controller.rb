6ba910bb85015b6d15387f35fa3e801f54a39a38
originalblend337 <originalblend337@gmail.com>
2013-11-01 12:26:07 -0400

    functioning dotenv

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,14 +5,15 @@ class WelcomeController < ApplicationController
 
   def callback
   	# Get access token from code param
-    response = RestClient.post("https://github.com/login/oauth/access_token", {client_id: GITHUB_CLIENT_ID, client_secret: GITHUB_CLIENT_SECRET, code: params["code"]}, { accept: :json })
+    response = RestClient.post("https://github.com/login/oauth/access_token", {client_id: ENV['GITHUB_CLIENT_ID'], client_secret: ENV['GITHUB_CLIENT_SECRET'], code: params["code"]}, { accept: :json })
   	parsed_response = JSON.parse(response)
   	# current_user is a method in devise / github_access_token to be a field in the user model
   	session[:github_access_token] = parsed_response["access_token"]
     client = Octokit::Client.new :access_token => parsed_response["access_token"]
-    client.user.login
+    user = client.user
+    user.login
 
-    User.create(name: client.user.name, login: client.user.login, location: client.user.location, email: client.user.email, github_access_token: parsed_response["access_token"])
+    User.create(name: user.name, login: user.login, location: user.location, email: user.email, github_access_token: parsed_response["access_token"])
 
   	redirect_to root_path
   end
