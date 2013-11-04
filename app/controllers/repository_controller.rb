81b747a6ff105cf0c7064494b47cae103157dc43
originalblend337 <originalblend337@gmail.com>
2013-11-04 15:11:15 -0500

    displaying doughnut and color

--- app/controllers/repository_controller.rb
+++ app/controllers/repository_controller.rb
@@ -9,7 +9,6 @@ class RepositoryController < ApplicationController
 
   		if Repository.check_cache(params[:user_name]) == true
   			@repositories = Repository.get_cached_repos(params[:user_name])
-  			binding.pry
   		else
   			@repositories = Repository.get_repos(params[:user_name], session[:github_access_token])
   			@repositories.each { |fetched_repository| Repository.create(name: fetched_repository[:name], description: fetched_repository[:description], language: fetched_repository[:language], owner: fetched_repository[:owner], avatar: fetched_repository[:avatar]) }
