41c503ad337547009ead5db4f66bfb832fd9d6b0
Tadashi Okazaki <originalblend337@gmail.com>
2014-11-07 00:56:55 -0500

    wip adding top nav bar

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -3,9 +3,9 @@ class WebsitesController < ApplicationController
 	def create
 		@website = current_user.websites.new(website_params)
 		if @website.save
-			redirect_to("/#{current_user.name}/settings")
+			redirect_to "/#{current_user.login}"
 		else
-			redirect_to("/#{current_user.name}/settings")
+			redirect_to "/#{current_user.login}"
 		end
 	end
 
